ba071c015b2fbd0f34221b5f71f725e8ed4b0efd
Hamza Ghazouani <hamouzpmp@gmail.com>
2022-11-21 00:32:17 +0100

    first commit

---
+++ laravel-9/app/Http/Controllers/APIController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class APIController extends Controller
+{
+    //
+
+public function getUsers()
+{
+$getUsers=User::get();
+return $getUsers;
+}
+}
---
+++ react-js/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Welcome to React-Laravel App</h1>
+
+      <Button>Click Here</Button>
+      
+    </div>
+  );
+}
+
+export default App;
