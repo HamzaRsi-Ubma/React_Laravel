6897352084a9a87bc3b614950075d4c6bb4c4b04
Hamza Ghazouani <hamouzpmp@gmail.com>
2023-01-17 00:53:38 +0100

    update

--- react-js/src/components/Account.js
+++ react-js/src/components/Account.js
@@ -6,14 +6,18 @@ export const Account = () => {
   const [id,setId]= useState(user.userDetails.id);
   const [name,setName]= useState(user.userDetails.name);
   const [address,setAddress]= useState(user.userDetails.address);
-
+  const [mobile,setMobile]= useState(user.userDetails.mobile);
+  const [shop,setShop]= useState(user.userDetails.shop);
   async function Update (){
     if(name=="")
     {
       alert("Please enter your Name");
     }else{
-      let item = {id,name,address}
-      let result = await fetch('http://127.0.0.1:8000/api/update',{
+
+
+      
+      let item = {id,name,address,mobile,shop}
+      let result = await fetch('http://127.0.0.1:8000/api/update-user',{
         method:'POST',
         headers:{
           "Content-type":"application/json"
@@ -22,7 +26,7 @@ export const Account = () => {
       });
 
       result= await result.json();
-      console.warn("result",result);
+      alert("Your account details successfully updated!");
     }
   }
   return (
@@ -34,9 +38,13 @@ export const Account = () => {
           <h2>{user.userDetails.name}</h2>
          <h5> Your account details are as below: </h5>
 
-         Name:<input className='form-control' type='text' onChange={(e)=>setName(e.target.value)}  value={name} placeholder={user.userDetails.name}/>
+         Name:<input className='form-control' type='text' onChange={(e)=>setName(e.target.value)}  value={name} />
          <br></br>
          Address:<input className='form-control' type='text' onChange={(e)=>setAddress(e.target.address)}  value={address}/>
+         <br></br>
+         Mobile:<input className='form-control' type='text' onChange={(e)=>setMobile(e.target.mobile)}  value={mobile}/>
+         <br></br>
+         Shop:<input className='form-control' type='text' onChange={(e)=>setShop(e.target.shop)}  value={shop}/>
          <br></br>
 
          <button className='form-control btn btn-primary' onClick={Update}>Update</button>
